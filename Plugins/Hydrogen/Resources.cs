62dbc5f95e2b53cbbfbf20ea2c23ceba694a76c2
Matthew Davey <matthew.davey@dotbunny.com>
2013-07-26 10:18:44 -0400

    Testing adding to a submodule.

---
+++ Plugins/Hydrogen/Resources.cs
@@ -0,0 +1,48 @@
+﻿#region Copyright Notice & License Information
+// 
+// Resources.cs
+//  
+// Author:
+//   Matthew Davey <matthew.davey@dotbunny.com>
+//
+// Copyright (C) 2013 dotBunny Inc. (http://www.dotbunny.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System;
+
+namespace Hydrogen
+{
+    /// <summary>
+    /// Additional static functions, constants and classes used to extend existing Resources support inside of Unity.
+    /// </summary>
+    public static class Resources
+    {
+		/// <summary>
+        /// Load the object of the specified type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <returns>Object</returns>
+        public static T Load<T>(string path) where T : UnityEngine.Object
+        {
+            return (T)UnityEngine.Resources.Load(path, typeof(T));
+        }
+	}
+}
