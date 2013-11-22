b4e8685384f221fa6d1d4159b5cff4deba020054
Matthew Davey <matthew.davey@dotbunny.com>
2013-11-22 00:49:46 -0500

    Initial implementation (still buggy) of web action system.

---
+++ Components/Hydrogen.Core/hWebPool.cs
@@ -0,0 +1,86 @@
+﻿#region Copyright Notice & License Information
+// 
+// hWebStack.cs
+//  
+// Author:
+//   Matthew Davey <matthew.davey@dotbunny.com>
+//
+// Copyright (C) 2013 dotBunny Inc. (http://www.dotbunny.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System;
+using UnityEngine;
+
+/// <summary>
+/// Hydrogen.Core.WebStack Instance
+/// </summary>
+[AddComponentMenu("Hydrogen/Web Pool Manager")]
+public sealed class hWebPool : Hydrogen.Core.WebPool
+{
+	/// <summary>
+	/// Internal fail safe to maintain instance across threads
+	/// </summary>
+	/// <remarks>
+	/// Multithreaded Safe Singleton Pattern
+	/// </remarks>
+	/// <description>
+	/// http://msdn.microsoft.com/en-us/library/ms998558.aspx
+	/// </description>
+	private static readonly System.Object _syncRoot = new System.Object();
+	
+	/// <summary>
+	/// Internal reference to the static instance of the web stack.
+	/// </summary>
+	private static volatile hWebPool _staticInstance;
+	
+	/// <summary>
+	/// Gets the Web Stack instance, creating one if none is found.
+	/// </summary>
+	/// <value>
+	/// The Object Pool
+	/// </value>
+	public static hWebPool Instance 
+	{
+		get {
+			if (_staticInstance == null) {				
+				lock (_syncRoot) {
+					_staticInstance = FindObjectOfType (typeof(hWebPool)) as hWebPool;
+					
+					// If we don't have it, lets make it!
+					if (_staticInstance == null) {
+						GameObject newWebPool = new GameObject("Web Pool");
+						newWebPool.AddComponent<hWebPool>();
+						_staticInstance = newWebPool.GetComponent<hWebPool>();	
+					}
+				}
+			}
+			return _staticInstance;
+		}
+	}
+
+	/// <summary>
+	/// Does an instance exist of this class?
+	/// </summary>
+	public static bool Exists()
+	{
+		if ( _staticInstance == null ) return false;
+		else return true;
+	}
+}
---
+++ Plugins/Hydrogen.Core/WebPoolItem.cs
@@ -0,0 +1,91 @@
+﻿#region Copyright Notice & License Information
+// 
+// WebCall.cs
+//  
+// Author:
+//   Matthew Davey <matthew.davey@dotbunny.com>
+//
+// Copyright (C) 2013 dotBunny Inc. (http://www.dotbunny.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Hydrogen.Core
+{
+	/// <summary>
+	/// An internal web call used within the Hydrogen Framework. 
+	/// </summary>
+	public class WebPoolItem : Hydrogen.Core.ObjectPoolItemBase
+	{
+		private int _hash;
+
+		public override void OnSpawned()
+		{
+			_hash = 0;
+		}
+
+		public override void OnDespawned ()
+		{
+			StopAllCoroutines();
+			_hash = 0;
+		}
+
+		public override void DespawnSafely()
+		{
+
+		}
+
+		public int Call(string URI, string contentType, string payload, string cookie, System.Action<int, string> callback)
+		{
+			_hash = (Time.time.ToString() + URI + payload + Random.Range(0,100)).GetHashCode();
+
+			StartCoroutine(GetReturnedText (URI, contentType, payload, cookie, callback));
+
+			return _hash;
+		}
+
+		public IEnumerator GetReturnedText(string URI, string contentType, string payload, string cookie, System.Action<int, string> callback)
+		{
+			// Message Data
+			byte[] postData = System.Text.Encoding.ASCII.GetBytes(payload.ToCharArray());
+		
+			// Process Headers
+			Hashtable headers = new Hashtable();
+
+			headers.Add("Content-Type", contentType);
+			headers.Add("Content-Length", postData.Length);
+			if ( cookie != null ) headers.Add("Cookie", cookie);
+
+			// Make the call
+			WWW newCall = new WWW(URI, postData);
+
+			yield return newCall;
+
+			while ( !newCall.isDone ) yield return new WaitForSeconds(0.01f);
+
+			// Callback!
+			callback(_hash, newCall.text);
+
+			hObjectPool.Instance.Despawn(this.gameObject, this.poolID);
+		}
+	}
+}
