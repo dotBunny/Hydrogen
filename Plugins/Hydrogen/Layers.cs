924a1e196ba6769a91f9a3f1c00d6ad38d38b027
Matthew Davey <matthew.davey@dotbunny.com>
2014-01-11 09:30:59 -0500

    Revert "Bump year!"
    This reverts commit 494a4db8d81c3515c1f91f81b4f0bdd77325523c.

--- Plugins/Hydrogen/Layers.cs
+++ Plugins/Hydrogen/Layers.cs
@@ -7,7 +7,7 @@
 //       Matthew Davey <matthew.davey@dotbunny.com>
 //
 // Copyright (c) 2012 PixelPlacement (http://pixelplacement.com/)
-// Copyright (c) 2014 dotBunny Inc. (http://www.dotbunny.com)
+// Copyright (c) 2013 dotBunny Inc. (http://www.dotbunny.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
